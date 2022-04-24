8d0458a1dc2725898781795640f13ea6668f140f
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-24 10:56:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+const length = document.querySelector(`.length`);
+const uppercase = document.querySelector(`.cb-uppercase`);
+const lowercase = document.querySelector(`.cb-lowercase`);
+const numbers = document.querySelector(`.cb-numbers`);
+const symbols = document.querySelector(`.cb-symbols`);
+const displayPassword = document.querySelector(`.display-password`);
+const buttonPassowrd = document.querySelector(`.button-password`);
+
+
